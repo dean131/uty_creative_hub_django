94cdbd7e9687f02d221dbc5c21bddfc6911c2d2a
dean131 <deanabdillah@gmail.com>
2024-02-12 22:07:06 +0700

    Add CRUD operations for Faculty model

--- base/api/views/faculty_views.py
+++ base/api/views/faculty_views.py
@@ -24,5 +24,40 @@ class FacultyModelViewSet(ModelViewSet):
             message="List of all faculties fetched successfully",
             data=serializer.data
         )
-        
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return CustomResponse.created(
+                message="Faculty berhasil ditambahkan",
+                data=serializer.data,
+                headers=headers
+            )
+        return CustomResponse.serializers_erros(serializer.errors)
+ 
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+
+            if getattr(instance, '_prefetched_objects_cache', None):
+                # If 'prefetch_related' has been applied to a queryset, we need to
+                # forcibly invalidate the prefetch cache on the instance.
+                instance._prefetched_objects_cache = {}
+
+            return CustomResponse.updated(
+                message="Faculty berhasil diupdate",
+                data=serializer.data
+            )
+        return CustomResponse.serializers_erros(serializer.errors)
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return CustomResponse.deleted(
+            message="Faculty berhasil dihapus"
+        )
 
