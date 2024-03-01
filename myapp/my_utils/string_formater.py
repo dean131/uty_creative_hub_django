2f3cc47b6008314fe4100e81bd00d941f036494e
dean131 <deanabdillah@gmail.com>
2024-03-01 08:15:39 +0700

    Add check for empty full_name in name_formater function

--- myapp/my_utils/string_formater.py
+++ myapp/my_utils/string_formater.py
@@ -2,6 +2,8 @@
 
 
 def name_formater(full_name):
+    if not full_name:
+        return ''
     full_name = full_name.split(' ')
     return full_name[1] if (len(full_name[0]) < 3 and len(full_name) > 1) else full_name[0]
 
